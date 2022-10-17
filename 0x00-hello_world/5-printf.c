231e89b4c3d11779a1ec749318a9c2647d112cb0
Akseph <omosehinakinola@gmail.com>
2022-10-17 06:57:10 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,11 @@
-#include <stdio.h>                                                                                                                                              
-
-/**                                                                                                                                                             
- *
- *  * main - print a line of code using puts                                                                                                                       
- *
- *   *                                                                                                                                                              
- *
- *    * Return: 0                                                                                                                                                    
- *
- *     */                                                                                                                                                             
-
-int main(void)                                                                                                                                                  
-
-{                                                                                                                                                               
-
-	        printf("\"Programming is like building a multilingual puzzle");                                                                                           
-
-		        return (0);                                                                                                                                             
-
+#include <stdio.h>
+/*
+* main - print a line
+* Return: 0
+*
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
