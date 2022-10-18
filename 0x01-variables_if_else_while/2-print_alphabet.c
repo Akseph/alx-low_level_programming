71a3ea25c8968f60bbdce61db50c89455d5e1257
Akseph <omosehinakinola@gmail.com>
2022-10-17 17:45:23 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+/**
+* main- work in progress
+*
+* Return: 0
+*
+*/
+
+
+	int main(void)
+{	char a;
+	for (a = 'a'; a <= 'z'; a++)
+
+{
+	putchar(a);
+}	putchar('\n');
+	return (0);
+
+}
