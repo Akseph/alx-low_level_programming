411762cb27636d0f78f3ca3d9b93dd6f62e6a4f8
Akseph <omosehinakinola@gmail.com>
2022-10-17 20:01:36 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 /**
-* main- print all possible combinations of two digits
+*main-  Print all possible combinations of two digits.
 * Return: 0
-*
 */
-
 int main(void)
 {
 	int i, j, k;
 
 	i = 0;
 	while (i < 100)
+{
 	j = i % 10; /* singles digit */
 	k = i / 10; /* doubles digit */
 	if (k < j)
@@ -18,13 +17,9 @@ int main(void)
 		putchar(k + '0');
 		putchar(j + '0');
 		if (i < 89)
-		{
 			putchar(44);
-			putchar(32);
-		}
-	}
-	i++;
-}
+		putchar(32); }
 	putchar('\n');
 	return (0);
-	}
+
+}
