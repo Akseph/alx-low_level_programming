05c7ab2de02733a89f032a6f1ab982aba1fa4d88
Akseph <omosehinakinola@gmail.com>
2022-10-20 04:35:51 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+#include "main.h"
+
+/**
+* _isupper- to check for upper case
+*@c: the character to be check;
+*Return: 1 if the character is upper case, 0 if the character is lower case
+*/
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_h
+
+
+
+
+/**
+ * file - main.h
+ *
+ */
+
+int _putchar(char c);
+
+
+
+
+
+#endif
