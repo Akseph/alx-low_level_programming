963218ffe7ad99fc19f4422c95862ff0097e1b9a
Akseph <omosehinakinola@gmail.com>
2022-10-23 22:48:15 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,15 @@
 */
 
 int main(void)
-{	
+{
 	char *s = "_putchar"; /*arrays of characters or strings*/
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		_putchar(s[i]);
 		i++;
 	}
 	_putchar('\n');
-	
 	return (0);
 }
