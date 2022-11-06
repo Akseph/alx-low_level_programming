65f94579408de872199ad0faa3760506d31c7c94
Akseph <111166164+Akseph@users.noreply.github.com>
2022-11-06 05:15:52 +0100

    final commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 /**
- *main - is a function that prints the name of the file
+ * main - is a function that prints the name of the file
  * @argc: is an argument counter for char
- *   @argv: is an argument value for char
+ * @argv: is an argument value for char
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
